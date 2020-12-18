e7c65511c995e41d4fe770f696315c2dcf4efdff
LAMurakami <GitHub@LAMurakami.com>
2020-12-18 11:06:23 -0900

    Move DocumentRoot on aws to match blinkenshell

--- blinkenshell_aws_rsync.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-host=`uname -n`              # Use host name as ssh-agent environment file name
-. ~/.ssh/agent/${host} >/dev/null           # source ssh-agent environment file
-
-rsync --archive --delete --rsh="ssh -q" \
-triton:/home/lam/public_html/ \
-/var/www/blinkenshell
